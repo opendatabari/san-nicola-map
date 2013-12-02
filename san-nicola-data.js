cc7c8c0a574a95274a18b49904b4f5eb95f79854
Simone Cortesi <simone@cortesi.com>
2013-12-02 11:45:11 +0100

    initial commit

---
+++ san-nicola-data.js
@@ -0,0 +1,9 @@
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+//[37.7825724, 15.2188840667, "27"],
+//[37.7788214833, 15.2211284667, "88"]
+var addressPoints = [
+[40.849080, 14.256765, "Chiesa di San Nicola a Nilo"],
+[40.393431, 8.705056, "Chiesa di San Nicola di Trullas"]
+];
+
